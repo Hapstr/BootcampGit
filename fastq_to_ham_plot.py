ed8e43ef9f8ad92cb4637f94414dd8ef5166aa8f
Calla Martyn <callamartyn@MacBook-Pro-2.ucsf.edu>
2018-09-06 11:43:03 -0700

    added fastq parser

--- fastq_to_ham_plot.py
+++ fastq_to_ham_plot.py
@@ -4,13 +4,19 @@
 # Script must ultimately be contained in one fluid .py file merged via GitHub
 
 import matplotlib.pyplot as plt
+from Bio import SeqIO
+import sys
 
-f = open(CTGATC.fastq)
+my_fastq = sys.argv[1]
 
 def getSeqs(fastq_file):
 	#Parse a FASTQ for sequence identities and corresponding sequences
-	seqences = {}
+	sequences = {}
+	for record in SeqIO.parse(fastq_file, "fastq"):
+	    sequences[record.name] = record.seq
 	return sequences
+
+sequences = getSeqs(my_fastq)
 
 def hamDist(str1, str2):
    #Count the # of differences between equal length strings str1 and str2
